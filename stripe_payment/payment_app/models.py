5bc2863ceb6506947bced8fc56d5c55ac3c70ac5
Alexander <job.nullptr@gmail.com>
2023-02-16 18:10:00 +0600

    Add Item model

--- stripe_payment/payment_app/models.py
+++ stripe_payment/payment_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """Модель товара"""
+    name = models.CharField(verbose_name='Название', max_length=255)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    price = models.IntegerField(verbose_name='Цена', default=0)
