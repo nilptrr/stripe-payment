452ddd9cb71ec2966f5e042f2742bfd753d00ca5
Alexander <job.nullptr@gmail.com>
2023-02-17 15:42:23 +0600

    api key to environment

--- stripe_payment/payment_app/stripe_service.py
+++ stripe_payment/payment_app/stripe_service.py
@@ -1,12 +1,11 @@
+import os
 import stripe
 from .models import Item
 
-API_KEY = 'sk_test_51MaGPSLi7UmMpAciemZZAalHC13fnQZPCNusK2q1Pw4Mm16t6TQecNpENe7gFnDgSxFOrvLXyF4AuOgLiO7yGUaT00uvyQ4kRv'
-# TODO вынести API_KET в ENV
 
-def create_chechout_session_id(item_id: int):
+def create_chechout_session_id(item_id: int) -> int:
     item = Item.objects.get(pk=item_id)
-    stripe.api_key = API_KEY
+    stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
     checkout_session = stripe.checkout.Session.create(
         line_items=[{
         'price_data': {
