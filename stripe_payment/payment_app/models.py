7d9ca72d90890fa7347605714c7373fe8d121944
Alexander <job.nullptr@gmail.com>
2023-02-18 15:32:55 +0600

    add get_price method to Item

--- stripe_payment/payment_app/models.py
+++ stripe_payment/payment_app/models.py
@@ -6,3 +6,9 @@ class Item(models.Model):
     name = models.CharField(verbose_name='Название', max_length=255)
     description = models.TextField(verbose_name='Описание', blank=True)
     price = models.IntegerField(verbose_name='Цена', default=0)
+
+    def __str__(self):
+        return self.name
+
+    def get_price(self):
+        return self.price / 100
