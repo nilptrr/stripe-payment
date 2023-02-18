68c965d48bc6c375a79fd372d3014c3f2bbe41dd
Alexander <job.nullptr@gmail.com>
2023-02-18 15:34:40 +0600

    Add ItemDetailView

--- stripe_payment/payment_app/urls.py
+++ stripe_payment/payment_app/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 from .views import CreateCheckoutSessionIdView, ItemDetailView
 
 urlpatterns = [
-    path('buy/<int:id>/', CreateCheckoutSessionIdView.as_view(), name='buy'),
-    path('item/<int:id>/', ItemDetailView.as_view(), name='item'),
+    path('buy/<int:pk>/', CreateCheckoutSessionIdView.as_view(), name='buy'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item'),
 ]
--- stripe_payment/payment_app/views.py
+++ stripe_payment/payment_app/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
 from django.http import JsonResponse
+import os
 
+from .models import Item
 from .stripe_service import create_chechout_session_id
 
 
 class CreateCheckoutSessionIdView(View):
     """Представление Session Id"""
     def get(self, request, *args, **kwargs):
-        session_id = create_chechout_session_id(kwargs['id'])
+        session_id = create_chechout_session_id(kwargs['pk'])
         return JsonResponse({
             'id': session_id
         })
 
 
-class ItemDetailView(View):
+class ItemDetailView(DetailView):
     """Детальное представление Item"""
+    model = Item
+    template_name = 'payment_app/checkout.html'
+    context_object_name = 'item'
     
+    def get_context_data(self, **kwargs):        
+        context = super(ItemDetailView, self).get_context_data(**kwargs)
+
+        STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+        context.update({
+            'STRIPE_PUBLIC_KEY': STRIPE_PUBLIC_KEY
+        })
+        
+        return context
